6bf2d86d2d8366aeea6af3e9c624ef12fadcb6d8
nylee <nylee226@gmail.com>
2022-04-13 21:21:22 +0900

    initial commit

---
+++ src/main/java/com/project/jdevone/JdevoneApplication.java
@@ -0,0 +1,13 @@
+package com.project.jdevone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdevoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdevoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/jdevone/JdevoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.jdevone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdevoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
